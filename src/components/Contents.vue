83135234db865b29f831903bf8f6f37b001de5ee
dkdk443 <demo_weeeds_470@yahoo.co.jp>
2021-02-20 00:59:53 +0900

    コンテンツの大枠実装

--- src/components/Contents.vue
+++ src/components/Contents.vue
@@ -6,11 +6,46 @@
         <hr>
         <div class="texts">
           <p>この度はこのポートフォリオサイト足を運んでいただき、誠にありがとうございます！</p>
-          <p>私は現在エンジニアを</p>
           <p>退屈されないように、たくさんの工夫を散りばめたいと思います。</p>
           <p>ごゆっくりとご覧ください。</p>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam fugit dolor maiores iure est doloribus nihil quia sapiente. Debitis cumque rerum assumenda vitae quasi iste similique, adipisci consectetur odio rem!</p>
         </div>
+
+      </div>
+      <div class="inner_content__box">
+        <h2>私について</h2>
+        <hr>
+        <div class="me-img">
+          <img src="/static/images/phone_hero.png" alt="" width="100px">
+        </div>
+        <div class="texts">
+          <p>広島県出身</p>
+          <p></p>
+        </div>
+      </div>
+      <div class="inner_content__box">
+        <h2>作ったもの</h2>
+        <hr>
+        <h3>人間関係を円滑にする類人猿分類</h3>
+        <div class="texts">
+          <p></p>
+          <p></p>
+        </div>
+      </div>
+      <div class="inner_content__box">
+        <h2>生い立ち</h2>
+        <hr>
+
+      </div>
+       <div class="inner_content__box">
+        <h2>連絡先</h2>
+        <hr>
+        <form action="">
+          <label for="email">メールアドレス</label>
+          <input type="email" name="email" id="email" placeholder="***@mail.com">
+          <textarea name="comment" id="comment" cols="30" rows="10"></textarea>
+          <input type="button" value="送信！">
+        </form>
 
       </div>
     </div>
