85ab154e3342125b6a6511eabd6eb8036bac8688
dkdk443 <demo_weeeds_470@yahoo.co.jp>
2021-02-20 00:44:10 +0900

    TOPにスクロールするボタンの実装

--- src/components/ScrollToTop.vue
+++ src/components/ScrollToTop.vue
@@ -1,11 +1,38 @@
 <template>
-  <div>
-    top
+  <div class="to-top-button" @click="scrollToTop()">
+    <i class="fas fa-caret-up fa-3x"></i>
   </div>
 </template>
 
 <script>
 export default {
-  name:'ScrollToTop'
+  name:'ScrollToTop',
+  methods: {
+    scrollToTop() {
+      window.scroll({
+        top:0,
+        behavior: "smooth"
+      });
+    }
+  },
 }
 </script>
+
+<style scoped lang="scss">
+  .to-top-button {
+    background-color: rgba(233, 233, 233, 0.665);
+    width: 60px;
+    height: 60px;
+    border-radius: 10px;
+    position: fixed;
+    bottom: 10px;
+    right: 10px;
+    display: flex;
+    justify-content: center;
+    .fas {
+      color: #CD8154;
+    }
+
+  }
+
+</style>
